bc5bf4ad6b178338dea249f2dd202a129714b953
skardas <skardas@gmail.com>
2021-03-07 01:43:18 +0300

    encryption service works well

--- src/protocols/encryption/encryption_service.rs
+++ src/protocols/encryption/encryption_service.rs
@@ -1,16 +1,17 @@
 #![allow(non_snake_case)]
 
+use std::ops::{ Mul};
+
+use openssl::bn::{BigNum, BigNumContext};
+use openssl::ec::{EcGroup, EcKey, EcPoint, PointConversionForm};
 use openssl::nid::Nid;
+use openssl::pkey::Private;
+use openssl::sha::sha256;
 use openssl::stack::Stack;
+use openssl::symm::{Cipher, decrypt_aead, encrypt_aead};
 use openssl::x509::*;
 use openssl::x509::store::X509StoreBuilder;
-use openssl::ec::{EcKey, EcGroup, EcPoint, PointConversionForm};
-use openssl::symm::{decrypt_aead, encrypt_aead, Cipher};
-use rand::{thread_rng, Rng};
-use openssl::pkey::Private;
-use openssl::bn::{BigNumContext, BigNum};
-use openssl::sha::sha256;
-use std::ops::{Mul, Add};
+use rand::{Rng, thread_rng};
 
 /// AES IV/nonce length
 pub const AES_IV_LENGTH: usize = 16;
@@ -22,7 +23,7 @@ pub const AES_IV_PLUS_TAG_LENGTH: usize = AES_IV_LENGTH + AES_TAG_LENGTH;
 pub const EMPTY_BYTES: [u8; 0] = [];
 
 
-pub fn encrypt(priv_a: &EcKey<Private>, sender_a: &X509, rec_b: &X509, message : &Vec<u8>) ->Option<EncryptedMessage> {
+pub fn encrypt(priv_a: &EcKey<Private>, sender_a: &X509, rec_b: &X509, message: &Vec<u8>) -> Option<EncryptedMessage> {
     let group = EcGroup::from_curve_name(Nid::SECP384R1).unwrap();
     let mut ctx = BigNumContext::new().unwrap();
 
@@ -30,27 +31,27 @@ pub fn encrypt(priv_a: &EcKey<Private>, sender_a: &X509, rec_b: &X509, message :
     let keyS = EcKey::generate(&group).unwrap();
 
     let Sp = keyS.public_key();
-    let S = Sp.to_bytes(&group,PointConversionForm::COMPRESSED, &mut ctx).unwrap();
+    let S = Sp.to_bytes(&group, PointConversionForm::COMPRESSED, &mut ctx).unwrap();
 
     let B = rec_b.public_key().unwrap();
     let B = B.ec_key().unwrap();
     let B = B.public_key();
 
     let mut Rprime = EcPoint::new(&group).unwrap();
-    if Rprime.mul(&group,B,keyR.private_key(),&mut ctx).is_err() {
-        return  None;
+    if Rprime.mul(&group, B, keyR.private_key(), &mut ctx).is_err() {
+        return None;
     }
 
-    let R = Rprime.as_ref().to_bytes(&group,PointConversionForm::COMPRESSED, &mut ctx).unwrap();
+    let R = Rprime.as_ref().to_bytes(&group, PointConversionForm::COMPRESSED, &mut ctx).unwrap();
 
     let key = sha256(R.as_slice());
-    let c = aes_encrypt(&key,message);
+    let c = aes_encrypt(&key, message);
     if c.is_none() {
-        return None
+        return None;
     }
     let c = c.unwrap();
 
-    let mut temp :Vec<u8> = vec![];
+    let mut temp: Vec<u8> = vec![];
     temp.extend(&S);
 
     let cn = sender_a.subject_name().entries_by_nid(Nid::COMMONNAME).next().unwrap();
@@ -61,39 +62,46 @@ pub fn encrypt(priv_a: &EcKey<Private>, sender_a: &X509, rec_b: &X509, message :
     let e = e.as_ref();
 
 
-    let z = e.mul(priv_a.private_key());
+    //z = e * a (A=aG)
+    let ea = e.mul(priv_a.private_key());
+    let mut z = BigNum::new().unwrap();
+
+    let mut order = BigNum::new().unwrap();
+    if group.order(&mut order, &mut ctx).is_err() {
+        return None;
+    }
+
+    //z = e * a + s = ea + s mod order
+    if z.mod_add(ea.as_ref(), keyS.private_key(), order.as_ref(), &mut ctx).is_err() {
+        return  None;
+    }
     let z = z.as_ref();
-    let z = z.add(keyS.private_key());
-    let z= z.as_ref();
-
-
-    let point = keyR.public_key();
-    let Rb = point.to_bytes(&group,PointConversionForm::COMPRESSED, &mut ctx).unwrap();
-    Some(EncryptedMessage{
-        R: Rb,
+    let rG =  keyR.public_key().to_bytes(&group, PointConversionForm::COMPRESSED, &mut ctx).unwrap();
+    Some(EncryptedMessage {
+        R: rG,
         C: c,
-        E:e.to_vec(),
+        E: e.to_vec(),
         Z: z.to_vec(),
     })
 }
 
-#[derive( Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct EncryptedMessage {
     R: Vec<u8>,
     C: Vec<u8>,
     E: Vec<u8>,
-    Z: Vec<u8>
+    Z: Vec<u8>,
 }
 
 impl EncryptedMessage {
-    pub fn getR(&self, group: &EcGroup) -> EcPoint{
+    pub fn getR(&self, group: &EcGroup) -> EcPoint {
         let mut ctx = BigNumContext::new().unwrap();
         EcPoint::from_bytes(&group, &self.R, &mut ctx).unwrap()
     }
-    pub fn getE(&self) -> BigNum{
+    pub fn getE(&self) -> BigNum {
         BigNum::from_slice(self.E.as_slice()).unwrap()
     }
-    pub fn getZ(&self) -> BigNum{
+    pub fn getZ(&self) -> BigNum {
         BigNum::from_slice(self.Z.as_slice()).unwrap()
     }
 }
@@ -106,26 +114,26 @@ pub fn decrypt(priv_b: &EcKey<Private>, sender_a: &X509, cipher: &EncryptedMessa
     let A = A.ec_key().unwrap();
 
     // check first e == H(zG − eA, A, C)
-    let mut S = EcPoint::new(&group).unwrap();
-    if S.mul_generator(&group,cipher.getZ().as_ref(),&mut ctx).is_err() {
+    let mut zG = EcPoint::new(&group).unwrap();
+    if zG.mul_generator(&group, cipher.getZ().as_ref(), &mut ctx).is_err() {
         return None;
     }
-
+    //eA = -e * A = -e*a*G
     let mut eA = EcPoint::new(&group).unwrap();
     if eA.mul(&group, A.public_key(), cipher.getE().as_ref(), &mut ctx).is_err() {
         return None;
     }
-    if eA.invert(&group,&mut ctx).is_err() {
+    if eA.invert(&group, &mut ctx).is_err() {
+        return None;
+    }
+    // S= zG -e A = sG
+    let mut S = EcPoint::new(&group).unwrap();
+    if S.add(&group, &zG, eA.as_ref(), &mut ctx).is_err() {
         return None;
     }
 
-    let mut S2 = EcPoint::new(&group).unwrap();
-    if S2.add(&group,&S, eA.as_ref(), &mut ctx).is_err() {
-        return None;
-    }
-
-    let str = S2.to_bytes(&group,PointConversionForm::COMPRESSED, &mut ctx).unwrap();
-    let mut temp :Vec<u8> = vec![];
+    let str = S.to_bytes(&group, PointConversionForm::COMPRESSED, &mut ctx).unwrap();
+    let mut temp: Vec<u8> = vec![];
     temp.extend(&str);
 
     let cn = sender_a.subject_name().entries_by_nid(Nid::COMMONNAME).next().unwrap();
@@ -136,12 +144,12 @@ pub fn decrypt(priv_b: &EcKey<Private>, sender_a: &X509, cipher: &EncryptedMessa
     }
     //now decrypt the message
     let mut Rprime = EcPoint::new(&group).unwrap();
-    if Rprime.mul(&group,cipher.getR(&group).as_ref(), priv_b.private_key(),&mut ctx) .is_err() {
+    if Rprime.mul(&group, cipher.getR(&group).as_ref(), priv_b.private_key(), &mut ctx).is_err() {
         return None;
     }
-    let rawKey = Rprime.as_ref().to_bytes(&group,PointConversionForm::COMPRESSED, &mut ctx).unwrap();
+    let rawKey = Rprime.as_ref().to_bytes(&group, PointConversionForm::COMPRESSED, &mut ctx).unwrap();
     let key = sha256(rawKey.as_slice());
-    aes_decrypt(&key,&cipher.C)
+    aes_decrypt(&key, &cipher.C)
 }
 
 pub fn verify_cert(cert: &[u8], ca: &[u8], common_name: &[u8]) -> Result<bool, String> {
